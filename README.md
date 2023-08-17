4a8dca90ebdd1c9df9fdc87dd6624305bae04160
Amaan <71920621+amaank404@users.noreply.github.com>
2023-08-17 23:30:35 +0530

    Added files

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# KindleMate-JP-EN-Dictionary
-A KindleMate dictionary for japanese and english both.
+# KindleMateAnki-JP-EN-Dictionary
+A KindleMateAnki dictionary for japanese and english both.
+
+## Screenshots of Cards
+
+
+
+## User Guide / Tutorial
+
+First head over to [releases](https://github.com/amaank404/KindleMateAnki-JP-EN-Dictionary/releases/latest) and from there download the latest `japanese-kindle-mate.txt`.
+
+Then do the following steps as described
+
+1. Open KindleMate with your kindle connected and import all your kindle vocabulary builder vocab
+
+![](scrshots/k1.png)
+
+2. Press `F12` or `Fn+F12` key depending on your keyboard to open dictionary menu, you can also open dictionary menu using the following
+
+![](scrshots/k2.png)
+
+3. Make sure you configure as follows, Change the dictionary, encoding, and make sure to tick `Ignore words with definition`. Then, choose what words you want to have the definations for, I recommend you choose vocabulary words since this will help you to fill out all unfilled definitions. Once that's done, press start and all the definitions will be filled with html. This itself is not readable within kindle mate. The HTML exists solely for anki cards.
+
+![](scrshots/k3.png)
+
+4. Export your vocabulary words either all of them or whatever you have selected
+
+![](scrshots/k4.png)
+
+5. Select the file type as `Anki (Tab separated, UTF-8, HTML enabled)(*.txt)`
+
+![](scrshots/k5.png)
+
+6. Enter your file name and hit save button, make sure you remember the filename and location whereever you are saving it.
+
+![](scrshots/k6.png)
+
+7. Head over to the following guide to setup your anki card and return back to this main page once that guide is done
+
+[LINK TO GUIDE](CREATING_ANKI_CARD.md)
+
+8. Once that's done, head over to Anki. Open it and create a new deck for all your current and future kindlemate card (You do not need to repeat this step when importing later on new cards, old card process isn't reset by anki on import)
+
+![](scrshots/y1.png)
+
+9. Now, Import the cards, using anki. Here point it to the location you specified earlier while exporting.
+
+![](scrshots/y2.png)
+![](scrshots/y3.png)
+
+10. Configure the import settings as follows, Field separator should always be set to Tab, make sure that the word is set to front and the sentence+defination (the second field) is set to Back in the field mapping options. Always, and I am saying always, check and make sure you are importing into correct deck, importing into another deck is half irreversable so better be safe than sorry. **Always check these settings carefully before importing**. Once that's done, hit import and let Anki do the rest for you.
+
+![](scrshots/y4.png)
+
+11. Now, you have got all you kindle words into anki with proper defination etc. Happy Learning!
+
--- kindlemateanki_jp_en_dictionary.py
+++ kindlemateanki_jp_en_dictionary.py
@@ -8,7 +8,7 @@ import requests
 
 def cvtjmdictprocessed_to_kindlemateanki(data: dict) -> bytes:
     # Converting it to Kindle Mate format
-    mainfmt = "{word}\t{define}"
+    mainfmt = "{word}\t{define}\r\n"
     outdict = []
 
     i2 = 0
@@ -19,6 +19,7 @@ def cvtjmdictprocessed_to_kindlemateanki(data: dict) -> bytes:
             print('-- Processed Words: ', i2)
         
         i2 += 1
+
         # Readings
         defination = "<div class=readingscont>"
         defination += "・".join([f"<span class=reading>{x}</span>" for x in item["reading"]])
@@ -81,7 +82,7 @@ def cvtjmdictprocessed_to_kindlemateanki(data: dict) -> bytes:
             defination += "</div></li>"
         
         defination += "</ol>"
-        outdict.append(mainfmt.format(word=k, define=defination) + "\n")
+        outdict.append(mainfmt.format(word=k, define=defination))
     return ''.join(outdict).encode('utf-8')
 
 def processjmdict(jmdict_data: dict) -> dict:
@@ -194,60 +195,64 @@ def processjmdict(jmdict_data: dict) -> dict:
             outprep[kana["text"]] = entry
     return outprep
 
+def downloadresources():
+    resp = requests.get("https://api.github.com/repos/scriptin/jmdict-simplified/releases/latest")
+
+    downloadsurl = {}
+    for x in resp.json()["assets"]:
+        if (fnmatch.fnmatch(x["name"], "jmdict-eng-?.*.json.zip") or 
+            fnmatch.fnmatch(x["name"], "jmnedict-all-?.*.json.zip")):
+
+            print(f"Queing download for asset: `{x['name']}`")
+            if x["name"].startswith("jmdict"):
+                downloadsurl["jmdict"] = x["browser_download_url"]
+            else:
+                downloadsurl["jmnedict"] = x["browser_download_url"]
+
+    print()
+    for k, v in downloadsurl.items():
+        print(f"Downloading {k} from {v}")
+        data = requests.get(v).content
+        print(f"Writing to disk")
+        os.makedirs("data/", exist_ok=True)
+        with open(f"data/{k}.zip", 'wb') as fp:
+            fp.write(data)
+        print(f"Extracting data")
+        file = zipfile.ZipFile(f"data/{k}.zip", 'r')
+        for x in file.namelist():
+            if x.endswith(".json"):
+                print("- Found json file within zip, reading it")
+                data2 = file.read(x)
+                print("- Writing to json file")
+                with open(f"data/{k}.json", "wb") as fp:
+                    fp.write(data2)
+                print("- Extraction Complete, Discarding ZIP File")
+                break
+        file.close()
+        os.remove(f"data/{k}.zip")
+        print(f"- Asset Download Complete: `{k}`")
+
+def jmdictRunner():
+    print("Processing JMDict - Phase 1")
+    print("- Reading and parsing JSON file")
+    with open("data/jmdict.json", "rb") as fp:
+        jmdict_data = json.loads(fp.read().decode("utf-8"))
+    print("- Processing")
+    processed_jmdict = processjmdict(jmdict_data)
+    print("Processing JMDict - Phase 2")
+    print("- Converting JSON Definations to Anki Readable HTML")
+    finaldict = cvtjmdictprocessed_to_kindlemateanki(processed_jmdict)
+    print("- Writing to `japanese-kindle-mate.txt`")
+    with open("japanese-kindle-mate.txt", "wb") as fp:
+        fp.write(finaldict)
+    print("- Copying japanese kindle mate file to local installation's dictionaries")
+    os.makedirs(os.path.expanduser("~")+"/Documents/Kindle Mate/dicts/", exist_ok=True)
+    shutil.copyfile("japanese-kindle-mate.txt", os.path.expanduser("~")+"/Documents/Kindle Mate/dicts/japanese-kindle-mate.txt")
+    print("Successfully Processed JMDict")
+
 print("Downloading the latest edition from scriptin/jmdict-simplified")
-
-resp = requests.get("https://api.github.com/repos/scriptin/jmdict-simplified/releases/latest")
-
-downloadsurl = {}
-for x in resp.json()["assets"]:
-    if (fnmatch.fnmatch(x["name"], "jmdict-eng-?.*.json.zip") or 
-        fnmatch.fnmatch(x["name"], "jmnedict-all-?.*.json.zip")):
-
-        print(f"Queing download for asset: `{x['name']}`")
-        if x["name"].startswith("jmdict"):
-            downloadsurl["jmdict"] = x["browser_download_url"]
-        else:
-            downloadsurl["jmnedict"] = x["browser_download_url"]
-
-print()
-for k, v in downloadsurl.items():
-    print(f"Downloading {k} from {v}")
-    data = requests.get(v).content
-    print(f"Writing to disk")
-    os.makedirs("data/", exist_ok=True)
-    with open(f"data/{k}.zip", 'wb') as fp:
-        fp.write(data)
-    print(f"Extracting data")
-    file = zipfile.ZipFile(f"data/{k}.zip", 'r')
-    for x in file.namelist():
-        if x.endswith(".json"):
-            print("- Found json file within zip, reading it")
-            data2 = file.read(x)
-            print("- Writing to json file")
-            with open(f"data/{k}.json", "wb") as fp:
-                fp.write(data2)
-            print("- Extraction Complete, Discarding ZIP File")
-            break
-    file.close()
-    os.remove(f"data/{k}.zip")
-    print(f"- Asset Download Complete: `{k}`")
+downloadresources()
 
 print()
 print("Starting Processing Phase")
-print("Processing JMDict - Phase 1")
-print("- Reading and parsing JSON file")
-with open("data/jmdict.json", "rb") as fp:
-    jmdict_data = json.loads(fp.read().decode("utf-8"))
-print("- Processing")
-processed_jmdict = processjmdict(jmdict_data)
-print("Processing JMDict - Phase 2")
-print("- Converting JSON Definations to Anki Readable HTML")
-finaldict = cvtjmdictprocessed_to_kindlemateanki(processed_jmdict)
-print("- Writing to `japanese-kindle-mate.txt`")
-with open("japanese-kindle-mate.txt", "wb") as fp:
-    fp.write(finaldict)
-print("- Copying japanese kindle mate file to local installation's dictionaries")
-os.makedirs(os.path.expanduser("~")+"/Documents/Kindle Mate/dicts/", exist_ok=True)
-shutil.copyfile("japanese-kindle-mate.txt", os.path.expanduser("~")+"/Documents/Kindle Mate/dicts/japanese-kindle-mate.txt")
-print("Successfully Processed JMDict")
-
+jmdictRunner()
